f751e1a41e4d22a646737c18f9471399c6257157
Harry James <harryjamesuk@users.noreply.github.com>
2016-03-28 16:41:40 +0100

    Started work on include separation
    Added inc (include) folder for includes. Added header here. index.php includes this header, rather than hard-coding it to one place.

---
+++ inc/header.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title><?php echo "Personal Media Library"; ?></title>
+<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+
+<div class="header">
+
+    <div class="wrapper">
+
+        <h1 class="branding-title"><a href="./">Personal Media Library</a></h1>
+
+        <ul class="nav">
+            <li class="books"><a href="catalog.php?cat=books">Books</a></li>
+            <li class="movies"><a href="catalog.php?cat=movies">Movies</a></li>
+            <li class="music"><a href="catalog.php?cat=music">Music</a></li>
+            <li class="suggest"><a href="suggest.php">Suggest</a></li>
+        </ul>
+
+    </div>
+
+</div>
+
+<div id="content">
--- index.php
+++ index.php
@@ -1,29 +1,5 @@
+<?php include("inc/header.php"); ?>
 
-<html>
-<head>
-	<title><?php echo "Personal Media Library"; ?></title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-</head>
-<body>
-
-	<div class="header">
-
-		<div class="wrapper">
-
-			<h1 class="branding-title"><a href="./">Personal Media Library</a></h1>
-
-			<ul class="nav">
-                <li class="books"><a href="catalog.php?cat=books">Books</a></li>
-                <li class="movies"><a href="catalog.php?cat=movies">Movies</a></li>
-                <li class="music"><a href="catalog.php?cat=music">Music</a></li>
-                <li class="suggest"><a href="suggest.php">Suggest</a></li>
-            </ul>
-
-		</div>
-
-	</div>
-
-	<div id="content">
 		<div class="section catalog random">
 
 			<div class="wrapper">
@@ -38,7 +14,7 @@
 
 		</div>
 
-	</div>
+	</div><!-- end content -->
 
 	<div class="footer">
 
