2148e0bcacd3dd40059cf3fab67afbd5692f1a83
Harry James <harryjamesuk@users.noreply.github.com>
2016-03-28 17:25:19 +0100

    Switching page title
    Based on category parameter appended to URL.

--- catalog.php
+++ catalog.php
@@ -1,11 +1,26 @@
 <?php
 
-$pageTitle = "Full Catalog";
+$pageTitle = "Full Catalog";    // Default title.
+if (isset($_GET['cat'])) { // A category has been supplied.
+    $cat = $_GET['cat'];    // Get the category.
+
+    switch ($cat) { // Switch based on category. Leave default if none of these.
+        case "books":
+            $pageTitle = "Books";
+            break;
+        case "movies":
+            $pageTitle = "Movies";
+            break;
+        case "music":
+            $pageTitle = "Music";
+            break;
+    }
+}
 
 include("inc/header.php"); ?>
 
 <div class="section page">
-    <h1>Full catalog</h1>
+    <h1><?php echo $pageTitle; ?></h1>
 </div>
 
 <?php include("inc/footer.php"); ?>
